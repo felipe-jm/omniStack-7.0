724af782b28801a4d609d09a7b8d4793fc01d102
Felipe Jung <felipemattoseu@gmail.com>
2019-06-24 16:20:09 -0400

    Some changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-mongoose.connect('mongodb+srv://{mongodbuser}:{mongodbpassword}@cluster0-h9m6o.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect('mongodblink', { useNewUrlParser: true });
 
 app.use((req, res, next) => {
     req.io = io;
